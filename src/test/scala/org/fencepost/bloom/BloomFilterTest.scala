9df7eb3475976d14062908e625b4b7438dedb393
Heuristic Fencepost <h.fencepost@gmail.com>
2011-04-11 23:59:02 -0500

    Changed function definition from the feedback-based implementation that was being used to a fixed set of functions using randomly generated seeds.  There seems to be a goodness in applying the same set of functions to each input although logically the feedback-based approach should work as well.

--- src/test/scala/org/fencepost/bloom/BloomFilterTest.scala
+++ src/test/scala/org/fencepost/bloom/BloomFilterTest.scala
@@ -9,19 +9,18 @@ class BloomFilterTest extends Suite {
 
   def testBasic() = {
 
-    val candidateCount = 2
+    val candidateCount = 5000
 
     // Generate set of candidates
     val candidates = for { i <- 1 to candidateCount } yield RandomStringUtils.randomAscii(24)
     val bloom = new BloomFilter(candidates,1)
-    println(bloom toString)
     
     // All candidates should be present
     val matches1 = candidates filter { arg => bloom contains arg }
     expect(candidateCount) { matches1.size }
 
     // Generate a set of new random strings
-    val somestrings = for { i <- 1 to 20 } yield RandomStringUtils.randomAscii(24)
+    val somestrings = for { i <- 1 to 5000 } yield RandomStringUtils.randomAscii(24)
     val matches2 = somestrings filter { arg => bloom contains arg }
     expect(0) { matches2.size }
   }
