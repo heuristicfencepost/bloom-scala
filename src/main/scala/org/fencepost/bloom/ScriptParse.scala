7acca3fae0c4d521896731acb2481cbc18c4cb44
Heuristic Fencepost <h.fencepost@gmail.com>
2011-04-26 23:01:06 -0500

    FSM-based parsing now appears to be working.  Current parsing rules require a word to be bigger than two characters... that needs to get cleaned up.

--- src/main/scala/org/fencepost/bloom/ScriptParse.scala
+++ src/main/scala/org/fencepost/bloom/ScriptParse.scala
@@ -6,44 +6,56 @@ import scala.xml.pull._
 // Object which implements logic for parsing a given Shakespearean play in
 // XML format.
 object ScriptParse {
-  
-  def getLines(path:String) = {
-    
-    def getText(reader:XMLEventReader):String = {
-      val event = reader.next
-      event match {
-        case EvText(txt) => txt
-        case EvElemStart(_,_,_,_) => {
-            reader.next // sub-element text event
-            reader.next // sub-element end event
-            getText(reader)
-        }
-        case EvElemEnd(_,_) => "" // can happen if STAGEDIR is only value for line
-      }
+
+  // We've seen a LINE element so handle it
+  def handleLine(reader:XMLEventReader):Stream[String] = {
+
+    reader.next match {
+
+      // straight text gets added to our stream.  We aren't done with the line,
+      // however, so we append the discovered text to a recursive call to
+      // ourselves.  An end event for the LINE element will return us to
+      // getLines()
+      case EvText(txt) => Stream.cons(txt,handleLine(reader))
+      case EvElemStart(_,"STAGEDIR",_,_) => handleStagedir(reader)
+        
+      // Entities can be safely ignored for our purposes
+      case EvEntityRef(_) => handleLine(reader)
+      case EvElemEnd(_,"LINE") => getLines(reader)
     }
-    
-    val reader = new XMLEventReader(Source.fromFile(path))
-    val urlines = reader map { event:XMLEvent =>
-      event match {
-        case EvElemStart(_,"LINE",_,_) => Some(getText(reader))
-        case _ => None
-      }
+  }
+
+  // We've seen a STAGEDIR so handle it
+  def handleStagedir(reader:XMLEventReader):Stream[String] = {
+
+    reader.next match {
+
+      // stage direction may have internal text.  If it does we aren't done
+      // handle the element yet so recursively call ourselves.  Otherwise
+      // exit out on an end element.
+      case EvText(txt) => handleStagedir(reader)
+      case EvElemEnd(_,"STAGEDIR") => getLines(reader)
     }
-    val lines = urlines filter { aval:Option[String] =>
-      aval match {
-        case Some(astr) => true
-        case None => false
+  }
+
+  // While there's still something to read iterate through the event stream
+  // until we find another LINE element.
+  def getLines(reader:XMLEventReader):Stream[String] = {
+
+      if (!reader.hasNext) { return Stream.Empty }
+      reader.next match {
+        case EvElemStart(_,"LINE",_,_) => handleLine(reader)
+        case _ => getLines(reader)
       }
-    }
-    lines map (_.get)
   }
 
   def getWords(path:String) = {
 
+    val reader = new XMLEventReader(Source.fromFile(path))
     val pruneRegex = """([a-zA-Z0-9]\S+[a-zA-Z0-9])""".r
     for {
-      line <- getLines(path)
-      word <- line.split("\\s").toIterator
+      line <- getLines(reader)
+      word <- line.split("\\s")
       prunedword = pruneRegex findFirstIn word
       if prunedword != None
     } yield prunedword.get
